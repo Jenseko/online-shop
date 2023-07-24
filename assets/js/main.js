fef60b96dccfbe72a1d22b265260260fbf300be0
Jenseko <jenscode23@gmail.com>
2023-07-24 14:34:25 +0200

    fixed

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+console.log("it works");
+
+let email = document.querySelector('section form input[type="email"]');
+
+function showEmail() {
+  email = console.log(email.value);
+}
+
+// const email = document.querySelector('footer form input[type="email"]');
+// console.log(email);
+
+// function showMail(){
+//     output = (email.value);
+//     console.log(output);
+//     location.href = "../../confirmation.html";
