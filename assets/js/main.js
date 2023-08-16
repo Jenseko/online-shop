9027423d8194be81c8b278101f2f35dbe9d36d99
Jenseko <jenscode23@gmail.com>
2023-08-16 23:01:55 +0200

    add some media query

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,5 @@
-console.log("it works");
-
-let email = document.querySelector('section form input[type="email"]');
+const email = document.querySelector('section form input[type="email"]');
 
 function showEmail() {
   email = console.log(email.value);
 }
-
-// const email = document.querySelector('footer form input[type="email"]');
-// console.log(email);
-
-// function showMail(){
-//     output = (email.value);
-//     console.log(output);
-//     location.href = "../../confirmation.html";
